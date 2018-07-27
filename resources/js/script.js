75366340d61b7fba10a264c11ec4410cd05ba578
Ewan Bauld <ewan.bauld@gmail.com>
2018-07-27 14:56:00 +0100

    Add JQuery Scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,18 @@
-'use strict';
-
-$(document).ready(() => {
+$(document).ready(function () {
     
-
+    /* for the sticky naviagation */
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');    
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+    
+    /* Scroll on buttons */
+    $('.js--scroll-to-plan').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);                     
+    });
 });
