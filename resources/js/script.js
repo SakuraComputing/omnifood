afdc710f2ec95733793a412f8e84e7d652488df2
Ewan Bauld <ewan.bauld@gmail.com>
2018-07-30 21:56:24 +0100

    Add google maps

--- resources/js/script.js
+++ resources/js/script.js
@@ -96,3 +96,10 @@ $(document).ready(function () {
             icon.removeClass('ion-close-round');
         }
     });
+
+    /* Maps */
+//    var map = new GMaps({
+//        div: '#map',
+//        lat: -12.043333,
+//        lng: -77.028333
+//    });
